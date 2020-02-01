1ecc22ebe568c41868b7f8046059af7c3405700d
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-01 18:11:11 +0700

    implement immer in reducer

--- app/store/AppReducer/index.js
+++ app/store/AppReducer/index.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {START_LOADING, STOP_LOADING} from './constants';
 
 const initialState = {
@@ -7,17 +8,20 @@ const initialState = {
   message: 'welcome to alpucart',
 };
 
-function AppReducer(state = initialState, action) {
-  switch (action.type) {
-    case START_LOADING:
-      return {...state, isLoading: true};
+const AppReducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case START_LOADING:
+        draft.isLoading = true;
+        break;
 
-    case STOP_LOADING:
-      return {...state, isLoading: false};
+      case STOP_LOADING:
+        draft.isLoading = false;
+        break;
 
-    default:
-      return state;
-  }
-}
+      default:
+        return state;
+    }
+  });
 
 export default AppReducer;
