163fe12c00e40a5e178a0511af38468de60efd0e
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-01 19:02:38 +0700

    implement stack navigation in app

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 
 import HomeScreen from '../../Screens/HomeScreen';
 import store from '../../store';
 
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
 function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <AppContainer />
     </Provider>
   );
 }
