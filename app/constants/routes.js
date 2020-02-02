8c6d2fcdcd14b7ae0000a21c3a5f24849b9de290
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-02 14:08:03 +0700

    register otp screen to

--- app/constants/routes.js
+++ app/constants/routes.js
@@ -3,22 +3,31 @@ import SplashScreen from '../Screens/SplashScreen';
 import LandingScreen from '../Screens/LandingScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import LoginScreen from '../Screens/LoginScreen';
+import OTPScreen from '../Screens/OTPScreen';
 
 const routes = {
   HomeScreen: {
     screen: HomeScreen,
   },
+
   LandingScreen: {
     screen: LandingScreen,
   },
+
   SplashScreen: {
     screen: SplashScreen,
   },
+
   RegisterScreen: {
     screen: RegisterScreen,
   },
+
   LoginScreen: {
     screen: LoginScreen,
+  },
+
+  OTPScreen: {
+    screen: OTPScreen,
   },
 };
 
