f2edce4c6605e69a2c3b79ba29e3e34de408ea8e
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-02 11:33:05 +0700

    create routes constants for route list

---
+++ app/constants/routes.js
@@ -0,0 +1,17 @@
+import HomeScreen from '../Screens/HomeScreen';
+import SplashScreen from '../Screens/SplashScreen';
+import LandingScreen from '../Screens/LandingScreen';
+
+const routes = {
+  HomeScreen: {
+    screen: HomeScreen,
+  },
+  LandingScreen: {
+    screen: LandingScreen,
+  },
+  SplashScreen: {
+    screen: SplashScreen,
+  },
+};
+
+export default routes;
