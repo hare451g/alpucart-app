3ed1067beb5f0f292afe173e02f3cf18fa256c06
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-02 12:40:31 +0700

    create typography component for controlling text

---
+++ app/components/Typography/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+const Typography = styled.Text`
+  font-family: 'Montserrat';
+  font-size: 16px;
+  font-weight: normal;
+`;
+
+export default Typography;
