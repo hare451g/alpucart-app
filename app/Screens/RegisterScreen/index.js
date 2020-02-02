15b90a4a629b317b843425f222e8792a8a708d64
hendra.sadewa <hendra.sadewa@hara.ag>
2020-02-02 12:46:13 +0700

    create register screen

---
+++ app/Screens/RegisterScreen/index.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {StatusBar} from 'react-native';
+import {} from 'react-native-material-ui';
+
+import AlpucartLogo from '../../images/alpucart-logo.png';
+
+import Button from '../../components/Button';
+
+import {
+  SafeAreaView,
+  ImageContainer,
+  Logo,
+  TextInputContainer,
+  TextInputLabel,
+  TextInput,
+  ButtonContainer,
+} from './styled';
+
+function RegisterScreen(props) {
+  const [isFocused, setFocus] = useState(false);
+
+  const onFocus = e => {
+    console.log(e.target);
+    setFocus(true);
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ImageContainer>
+          <Logo source={AlpucartLogo} />
+        </ImageContainer>
+
+        <TextInputContainer isFocused={isFocused}>
+          <TextInputLabel isFocused={isFocused}>Nama Lengkap</TextInputLabel>
+          <TextInput onFocus={onFocus} isFocused={isFocused} />
+        </TextInputContainer>
+
+        <ButtonContainer>
+          <Button isOutline> Login </Button>
+        </ButtonContainer>
+      </SafeAreaView>
+    </>
+  );
+}
+
+RegisterScreen.navigationOptions = () => ({
+  title: 'Registrasi Akun',
+});
+
+export default RegisterScreen;
