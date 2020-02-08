b52e19ebf08579622def31116ee167f5c636bc13
hare451g <hendrasadewa30@gmail.com>
2020-02-08 17:46:53 +0700

    create styles for modal header and product info

---
+++ app/components/ProductModal/styled.js
@@ -0,0 +1,70 @@
+import {Dimensions} from 'react-native';
+import styled from 'styled-components/native';
+
+const ModalContainer = styled.View`
+  height: 469px;
+  background-color: #fff;
+  border-radius: 8px;
+  margin: ${(Dimensions.get('window').height - 469) / 2}px 16px;
+`;
+
+const ModalContent = styled.View`
+  margin: 16px;
+`;
+
+const ModalHeader = styled.TouchableHighlight`
+  padding: 4px 8px;
+  display: flex;
+  align-self: flex-end;
+`;
+
+const ProductInfoContainer = styled.View`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: row;
+`;
+
+const ProductInfo = styled.View`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-direction: column;
+  max-width: 240px;
+  padding: 8px 16px;
+`;
+
+const ProductImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 8px;
+`;
+
+const ProductName = styled.Text`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 21px;
+`;
+
+const ProductPrice = styled.Text`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 18px;
+  color: #e80e0e;
+  margin-top: 16px;
+`;
+
+export {
+  ModalContainer,
+  ModalContent,
+  ModalHeader,
+  ProductInfoContainer,
+  ProductImage,
+  ProductInfo,
+  ProductName,
+  ProductPrice,
+};
