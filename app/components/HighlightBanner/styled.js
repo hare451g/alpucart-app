c6cf48443dd0e0202a9c054d64a384b3f6f38e28
hare451g <hendrasadewa30@gmail.com>
2020-02-08 15:23:25 +0700

    create highlight banner styled-component

---
+++ app/components/HighlightBanner/styled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components/native';
+import Typography from '../Typography';
+
+const TouchableContainer = styled.TouchableOpacity`
+  min-width: 328px;
+  min-height: 100px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  ${props => props.isSelected && `background-color: #2eab53;`}
+`;
+
+const Background = styled.ImageBackground`
+  min-width: 328px;
+  min-height: 100px;
+  border-radius: 8px;
+`;
+
+const TextContainer = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HighlightText = styled(Typography)`
+  font-size: 12px;
+  font-family: 'Montserrat_semi_bold';
+  color: #ffffff;
+  text-align: center;
+  padding: 6px;
+  min-width: 224px;
+`;
+
+const ButtonText = styled(Typography)`
+  font-size: 14px;
+  font-family: 'Montserrat_semi_bold';
+  color: #ffffff;
+  background-color: #030303;
+  padding: 6px;
+  border-radius: 8px;
+`;
+
+export {
+  TouchableContainer,
+  Background,
+  TextContainer,
+  HighlightText,
+  ButtonText,
+};
