e01125497329a5b55ff4c928621c40bc6e0e5d9c
hare451g <hendrasadewa30@gmail.com>
2020-02-09 01:35:45 +0700

    create header component for home

--- app/screens/HomeScreen/index.js
+++ app/screens/HomeScreen/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
+import {SafeAreaView, ScrollView, View, StatusBar, Image} from 'react-native';
+
 import HighlightBanner from '../../components/HighlightBanner';
+import ProductModal from '../../components/ProductModal';
 
 function HomeScreen() {
   return (
     <>
       <StatusBar barStyle="light-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>Alpucart</Text>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={{backgroundColor: '#fff'}}
+        >
+          <View style={{margin: 16}}>
             <HighlightBanner
               highlightText="Order apapun lebih mudah hanya dengan foto daftar belanja"
               buttonText="Upload Daftar Belanja"
               image="https://source.unsplash.com/random/328x100"
             />
+            <ProductModal />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -23,7 +28,16 @@ function HomeScreen() {
 }
 
 HomeScreen.navigationOptions = () => ({
-  headerShown: false,
+  headerTitle: () => (
+    <Image
+      source={require('../../images/alpucart-logo.png')}
+      style={{
+        width: 90,
+        height: 30.43,
+        alignSelf: 'flex-start',
+      }}
+    />
+  ),
 });
 
 export default HomeScreen;
